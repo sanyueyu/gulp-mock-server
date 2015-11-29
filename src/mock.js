e1521095e0e16bd2628a50da52210e00930f2cdf
sanyueyu <sanyueyu@gmail.com>
2015-11-29 10:30:05 +0800

    init

--- src/mock.js
+++ src/mock.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var path = require('path');
 var rd = require('rd');
 var CWD = process.cwd();
+var checkMark = /\.json/;
 
 module.exports = function(req, res, next){
   // mt is mocktag 作为标示,相同url参数不同请求的数据不同
@@ -11,9 +12,9 @@ module.exports = function(req, res, next){
   var fullName = urlObj.pathname + mt;
   var dir = path.join(CWD, './data/');
   var fileNames = rd.readSync(dir)
-    .filter(function(x) {return x.split('.')[1] === 'json'})
+    .filter(function(x) {return checkMark.test(x);})
     .map(function(x) {return x.split(path.sep + 'data' + path.sep)[1];})
-    .map(function(x) {return '/' + x.split('.')[0];});
+    .map(function(x) {return '/' + x.replace(checkMark, '');});
   var hasFile = false;
 
   // 查找本地是否存在这个文件
@@ -23,12 +24,14 @@ module.exports = function(req, res, next){
           hasFile = true;
       }
   });
+
   // 拦截到请求里本地存在这个文件
   if (hasFile) {
       var filePath = path.join(CWD, './data/' + fullName + '.json');
       console.log('[gulp-mock-server]', req.url + '=>' + filePath);
       // 删除缓存区里的某个模块 删除该模块后，下次加载该模块时重新运行该模块
       delete require.cache[require.resolve(filePath)];
+      res.setHeader("Access-Control-Allow-Origin", "*");
       if (urlObj.query&&urlObj.query.callback) {
           res.setHeader('Content-type', 'application/javascript');
           res.end(urlObj.query.callback + '(' + JSON.stringify(require(filePath)) + ')');
