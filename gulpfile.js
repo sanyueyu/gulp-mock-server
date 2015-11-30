a449679b393337c14fe2fefe3dfbd77143822276
普修 <xianming.zxm@alibaba-inc.com>
2015-11-30 10:26:11 +0800

    init

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,6 @@ var mockServer = require('./src/index');
 gulp.task('mock', function() {
     gulp.src('.')
     .pipe(mockServer({
-        livereload: {
-            enable: false
-        },
-        directoryListing: {
-            enable: true,
-            path: 'data'
-        },
         port: 8090,
         open: "http://localhost:8090/test"
     }));
