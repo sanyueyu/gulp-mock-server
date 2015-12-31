33d543e7f5e94956ac8a1fd03d91717ac545153b
sanyueyu <sanyueyu@gmail.com>
2015-12-31 08:39:19 +0800

    set mockdir

--- src/mock.js
+++ src/mock.js
@@ -5,75 +5,79 @@ var rd = require('rd');
 var CWD = process.cwd();
 var checkMark = /\.json|\.js/;
 
-module.exports = function(req, res, next){
-  // mt is mocktag 作为标示,相同url参数不同请求的数据不同
-  var mt = req.query.mt || req.body.mt || '';
-  var urlObj = url.parse(req.url, true);
-  var fullName = urlObj.pathname + mt;
-  var dir = path.join(CWD, './data/');
-  var fileNames = rd.readSync(dir)
-    .filter(function(x) {return checkMark.test(x);})
-    .map(function(x) {return x.split(path.sep + 'data' + path.sep)[1];})
-    .map(function(x) {
-      return {
-        name: '/' + x.replace(checkMark, ''),
-        verb: x.match(checkMark)[0]
-      };
-      //return '/' + x.replace(checkMark, '');
+module.exports = function(mockDir) {
+  return function(req, res, next){
+    // mt is mocktag 作为标示,相同url参数不同请求的数据不同
+    var mt = req.query.mt || req.body.mt || '';
+    var urlObj = url.parse(req.url, true);
+    var fullName = urlObj.pathname + mt;
+    //var dir = path.join(CWD, './data/');
+    var dir = path.join(CWD, mockDir);
+    var fileNames = rd.readSync(dir)
+      .filter(function(x) {return checkMark.test(x);})
+      //.map(function(x) {return x.split(path.sep + 'data' + path.sep)[1];})
+      .map(function(x) {return x.split(path.sep + mockDir.replace(/^\.\//, '').replace(/\//g, path.sep) + path.sep)[1];})
+      .map(function(x) {
+        return {
+          name: '/' + x.replace(checkMark, ''),
+          verb: x.match(checkMark)[0]
+        };
+      });
+    var hasFile = false;
+    var verb = 'json'; // 文件后缀
+
+    // 查找本地是否存在这个文件
+    fileNames.forEach(function(name) {
+        var _name = name.name.replace(new RegExp('\\' + path.sep, 'g'), '/');
+        if (fullName === _name) {
+            hasFile = true;
+            verb = name.verb;
+        }
     });
-  var hasFile = false;
-  var verb = 'json'; // 文件后缀
 
-  // 查找本地是否存在这个文件
-  fileNames.forEach(function(name) {
-      var _name = name.name.replace(new RegExp('\\' + path.sep, 'g'), '/');
-      if (fullName === _name) {
-          hasFile = true;
-          verb = name.verb;
-      }
-  });
+    // 拦截到请求里本地存在这个文件
+    if (hasFile) {
+        var inlineData; // 写在check条件里的数据
+        var filePath = path.join(dir + fullName + verb);
 
-  // 拦截到请求里本地存在这个文件
-  if (hasFile) {
-      var inlineData; // 写在check条件里的数据
-      var filePath = path.join(CWD, './data/' + fullName + verb);
+        if (verb === '.js') {
+          delete require.cache[require.resolve(filePath)];
+          var checks = require(filePath);
+          checks.forEach(function(item) {
+            var isCheck = true;
+            for (var k in item.params) {
+              if (item.params[k] != req.query[k]&&item.params[k] != req.body[k]) {
+                isCheck = false;
+              }
+            }
+            if (isCheck) {
+              if (typeof(item.response) === 'string') {
+                //filePath = path.join(dir + fullName.replace(/^\.|\/.+$/, '') + item.response);
+                filePath = path.join(dir + item.response.replace(/^\./, ''));
+              } else {
+                inlineData = item.response;
+              }
+              return;
+            }
+          });
+        }
 
-      if (verb === '.js') {
+        // 如果是JS文件则根据参数请求文件
+        console.log('[gulp-mock-server]', req.url + '=>' + filePath);
+
+        // 删除缓存区里的某个模块 删除该模块后，下次加载该模块时重新运行该模块
         delete require.cache[require.resolve(filePath)];
-        var checks = require(filePath);
-        checks.forEach(function(item) {
-          var isCheck = true;
-          for (var k in item.params) {
-            if (item.params[k] != req.query[k]&&item.params[k] != req.body[k]) {
-              isCheck = false;
-            }
-          }
-          if (isCheck) {
-            if (typeof(item.response) === 'string') {
-              filePath = path.join(CWD, '/data/' + fullName.replace(/\/.+$/, '') + item.response);
-            } else {
-              inlineData = item.response;
-            }
-            return;
-          }
-        });
-      }
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        if (urlObj.query&&urlObj.query.callback) {
+            res.setHeader('Content-type', 'application/javascript');
+            res.end(urlObj.query.callback + '(' + JSON.stringify(inlineData || require(filePath)) + ')');
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(inlineData || require(filePath)));
+        }
+        return;
+    }
 
-      // 如果是JS文件则根据参数请求文件
-      console.log('[gulp-mock-server]', req.url + '=>' + filePath);
-
-      // 删除缓存区里的某个模块 删除该模块后，下次加载该模块时重新运行该模块
-      delete require.cache[require.resolve(filePath)];
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      if (urlObj.query&&urlObj.query.callback) {
-          res.setHeader('Content-type', 'application/javascript');
-          res.end(urlObj.query.callback + '(' + JSON.stringify(inlineData || require(filePath)) + ')');
-      } else {
-          res.setHeader('Content-Type', 'application/json');
-          res.end(JSON.stringify(inlineData || require(filePath)));
-      }
-      return;
+    next();
   }
-
-  next();
 };
