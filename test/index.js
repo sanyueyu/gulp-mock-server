ae1cc097dc8d27f6257fb10c11ce3346315e0427
普修 <xianming.zxm@alibaba-inc.com>
2016-01-26 17:33:59 +0800

    new feature

--- test/index.js
+++ test/index.js
@@ -6,6 +6,7 @@ var testDoJSON = require('../data/test.do.json');
 var testJSON1 = require('../data/test1');
 var xiaoming = require('../data/class/xiaoming');
 var shanghai = require('../data/huang/yangguo/shanghai');
+var checkOne = require('../data/check_one.json');
 var url = 'http://localhost:8000';
 
 describe('gulp-mock-serve', function(){
@@ -49,6 +50,17 @@ describe('gulp-mock-serve', function(){
         });
   });
 
+  it('mock get /check.do?id=123 =>  check_one.json', function(done) {
+      stream = webserver();
+      request(url)
+        .post('/check.do')
+        .send({'id': '123'})
+        .expect(200, checkOne)
+        .end(function(err) {
+          if (err) return done(err);
+          done(err);
+        });
+  });
   it('mock post /test.do =>  test.do.json', function(done) {
       stream = webserver();
       request(url)
