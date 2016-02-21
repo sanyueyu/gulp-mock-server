4eb84d4ce318389376d641d73d1c0aa42f8a74a8
sanyueyu <sanyueyu@gmail.com>
2016-02-21 12:17:06 +0800

    init

--- data/check.htm.js
+++ data/check.htm.js
@@ -1,5 +1,5 @@
-module.exports = [{
+module.exports = {
   response: {
     name: "three"
   }
-}];
+};
--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ gulp.task('mock', function() {
     gulp.src('.')
     .pipe(mockServer({
         port: 8090,
-        open: "http://localhost:8090/test"
+        //open: "http://localhost:8090/test"
     }));
 });
 
