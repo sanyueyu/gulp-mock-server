024386b7603998c020b7dcc4299f376d5b7022d9
sanyueyu <sanyueyu@gmail.com>
2016-05-06 23:58:24 +0800

    support more powerful commandline

--- src/command.js
+++ src/command.js
@@ -1,9 +1,27 @@
 var path = require('path');
+var fs = require('fs');
+var fse = require('fs-extra');
+var request = require('request');
 
 var server = require('./index.js');
+var CWD = process.cwd();
 
 module.exports = {
   exec: function(argv) {
+    if (argv.fetch || argv.f) {
+      var mockDir = argv.server || argv.e;
+      mockDir = typeof(mockDir) === 'string' ? mockDir : './data';
+
+      var dir = path.join(CWD, mockDir);
+      var url = argv.f;
+      if (!url) return;
+
+      var arr = url.split('/');
+      // 去除 "http:", "", "hostname",
+      var name = arr.pop(dir, arr.slice(3).join('/'));
+      fse.ensureDirSync(path.join(dir, arr.slice(3).join('/')));
+      request(url).pipe(fs.createWriteStream(path.join(dir, arr.slice(3).join('/'), name + '.json')));
+    }
     if (argv.server || argv.e) {
       var mockDir = argv.server || argv.e;
       mockDir = typeof(mockDir) === 'string' ? mockDir : './data';
